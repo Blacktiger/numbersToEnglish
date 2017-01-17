0073ca811be1dedc1ac6ea87e925bc9a21baecfd
Michael Martz <blktiger@gmail.com>
2017-01-16 21:20:43 -0700

    Move conditional logic into list access

--- src/main/java/NumberUtils.java
+++ src/main/java/NumberUtils.java
@@ -1,10 +1,16 @@
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class NumberUtils {
 	private static final BigInteger ONE_HUNDRED = new BigInteger("100");
+	private static final List<String> ONES = Arrays.asList("", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+	private static final List<String> TEENS = Arrays.asList("ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen");
+	private static final List<String> TENS = Arrays.asList("", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety");
+	private static final List<String> SCALES = Arrays.asList("", " thousand ", " million ", " billion ", " trillion ", " quadrillion ", " quintillion ", " sextillion ");
+
 
 	public static String toEnglish(BigInteger number) {
 		if (BigInteger.ZERO.equals(number)) {
@@ -27,110 +33,13 @@ class NumberUtils {
 			String componentResult = hundredsToEnglish(componentNumber);
 			builder.append(componentResult);
 			if (!BigInteger.ZERO.equals(componentNumber)) {
-				if (i == 1) {
-					builder.append(" thousand ");
-				} else if (i == 2) {
-					builder.append(" million ");
-				} else if (i == 3) {
-					builder.append(" billion ");
-				} else if (i == 4) {
-					builder.append(" trillion ");
-				} else if (i == 5) {
-					builder.append(" quadrillion ");
-				} else if (i == 6) {
-					builder.append(" quintillion ");
-				} else if (i == 7) {
-					builder.append(" sextillion ");
-				}
+				builder.append(SCALES.get(i));
 			}
 		}
 
 		StringUtils.trimTrailingSpace(builder);
 
 		return builder.toString();
-	}
-
-	private static boolean isTen(BigInteger tens) {
-		return tens.equals(BigInteger.ONE);
-	}
-
-	private static boolean isTens(BigInteger tens) {
-		return tens.compareTo(BigInteger.ONE) > 0;
-	}
-
-	private static String onesToEnglish(BigInteger number) {
-		if (BigInteger.ONE.equals(number)) {
-			return "one";
-		} else if (new BigInteger("2").equals(number)) {
-			return "two";
-		} else if (new BigInteger("3").equals(number)) {
-			return "three";
-		} else if (new BigInteger("4").equals(number)) {
-			return "four";
-		} else if (new BigInteger("5").equals(number)) {
-			return "five";
-		} else if (new BigInteger("6").equals(number)) {
-			return "six";
-		} else if (new BigInteger("7").equals(number)) {
-			return "seven";
-		} else if (new BigInteger("8").equals(number)) {
-			return "eight";
-		} else if (new BigInteger("9").equals(number)) {
-			return "nine";
-		} else {
-			return "";
-		}
-	}
-
-	private static String teensToEnglish(BigInteger number) {
-		if (new BigInteger("10").equals(number)) {
-			return "ten";
-		} else if (new BigInteger("11").equals(number)) {
-			return "eleven";
-		} else if (new BigInteger("12").equals(number)) {
-			return "twelve";
-		} else if (new BigInteger("13").equals(number)) {
-			return "thirteen";
-		} else if (new BigInteger("14").equals(number)) {
-			return "fourteen";
-		} else if (new BigInteger("15").equals(number)) {
-			return "fifteen";
-		} else if (new BigInteger("16").equals(number)) {
-			return "sixteen";
-		} else if (new BigInteger("17").equals(number)) {
-			return "seventeen";
-		} else if (new BigInteger("18").equals(number)) {
-			return "eighteen";
-		} else {
-			return "nineteen";
-		}
-	}
-
-	private static String tensToEnglish(BigInteger tens, BigInteger ones) {
-		StringBuilder result = new StringBuilder();
-		if (tens.equals(new BigInteger("2"))) {
-			result.append("twenty");
-		} else if (tens.equals(new BigInteger("3"))) {
-			result.append("thirty");
-		} else if (tens.equals(new BigInteger("4"))) {
-			result.append("forty");
-		} else if (tens.equals(new BigInteger("5"))) {
-			result.append("fifty");
-		} else if (tens.equals(new BigInteger("6"))) {
-			result.append("sixty");
-		} else if (tens.equals(new BigInteger("7"))) {
-			result.append("seventy");
-		} else if (tens.equals(new BigInteger("8"))) {
-			result.append("eighty");
-		} else if (tens.equals(new BigInteger("9"))) {
-			result.append("ninety");
-		}
-
-		if (!BigInteger.ZERO.equals(ones)) {
-			result.append("-").append(onesToEnglish(ones));
-		}
-
-		return result.toString();
 	}
 
 	private static String hundredsToEnglish(BigInteger number) {
@@ -143,7 +52,7 @@ class NumberUtils {
 		BigInteger ones = number.mod(ONE_HUNDRED).mod(BigInteger.TEN);
 
 		StringBuilder builder = new StringBuilder("");
-		if (hundreds.compareTo(BigInteger.ZERO) > 0) {
+		if (isHundreds(hundreds)) {
 			builder.append(onesToEnglish(hundreds)).append(" hundred ");
 		}
 
@@ -158,5 +67,36 @@ class NumberUtils {
 		StringUtils.trimTrailingSpace(builder);
 
 		return builder.toString();
+	}
+
+	private static boolean isHundreds(BigInteger hundreds) {
+		return hundreds.compareTo(BigInteger.ZERO) > 0;
+	}
+
+	private static boolean isTen(BigInteger tens) {
+		return tens.equals(BigInteger.ONE);
+	}
+
+	private static boolean isTens(BigInteger tens) {
+		return tens.compareTo(BigInteger.ONE) > 0;
+	}
+
+	private static String tensToEnglish(BigInteger tens, BigInteger ones) {
+		StringBuilder result = new StringBuilder();
+		result.append(TENS.get(tens.intValue()));
+
+		if (!BigInteger.ZERO.equals(ones)) {
+			result.append("-").append(onesToEnglish(ones));
+		}
+
+		return result.toString();
+	}
+
+	private static String teensToEnglish(BigInteger number) {
+		return TEENS.get(number.intValue() - 10);
+	}
+
+	private static String onesToEnglish(BigInteger ones) {
+		return ONES.get(ones.intValue());
 	}
 }
